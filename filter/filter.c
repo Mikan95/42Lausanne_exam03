51c289d0bd582eaae117be435fb9d5d89c1bdef9
Mikan95 <andrewmeechan@hotmail.com>
2024-11-27 20:12:50 +0100

    Updated filter.c with latest constraints

--- filter/filter.c
+++ filter/filter.c
@@ -3,143 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   filter.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ameechan <ameechan@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 22:19:56 by ameechan          #+#    #+#             */
-/*   Updated: 2024/11/19 16:31:43 by ameechan         ###   ########.fr       */
+/*   Updated: 2024/11/27 20:11:42 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #define _GNU_SOURCE
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
-
-// /**
-//  * @brief Allocates memory big enough to copy contents of `str` to `res`.
-//  * @return A pointer to the newly allocated memory.
-//  */
-// char	*copy_str(char *str)
-// {
-// 	char	*res;
-// 	int		i = 0;
-
-// 	if (!str)
-// 		return (NULL);
-// 	res = malloc(sizeof(char) * strlen(str) + 1);
-// 	if (!res)
-// 		return (NULL);
-// 	while (str[i])
-// 	{
-// 		res[i] = str[i];
-// 		i++;
-// 	}
-// 	res[i] = '\0';
-// 	return (res);
-// }
-
-// /**
-//  * @brief Prints `str` to stdout.
-//  */
-// void	put_stdout(char *str)
-// {
-// 	int	i = 0;
-
-// 	while (str[i])
-// 	{
-// 		write(1, &str[i], 1);
-// 		i++;
-// 	}
-// 	write(1, "\n", 1);
-// }
-
-// /**
-//  * @brief Counts first, then replaces all occurences of `ndl` in `hs`.
-//  */
-// void	str_replace(char **hs, int hs_len, char *ndl, int ndl_len)
-// {
-// 	int		ndl_count = 0; // Number of occurences of `ndl` in `hs`
-// 	char	*ptr;	// Pointer to the first occurence of `ndl` in `hs`
-// 	char	*start = *hs;	// Pointer to the start of `hs`
-// 	int		i = 0;
-
-// 	ptr = *hs;	// Set the pointer to the start of `hs`
-// 	if (!memmem(*hs, hs_len, ndl, ndl_len))	// return; if `ndl` is not in `hs`
-// 		return ;
-
-// 	//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&//
-// 	//&&&&&&&&&&&&&  COUNT OCCURENCES OF `ndl` IN `hs`  &&&&&&&&&&&&&&&&&&//
-// 	//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&//
-// 	// (ptr - start) gives the len between start and ptr
-// 	// thus, by subtracting this from hs_len, we get the remaining length of the string.
-// 	while (memmem(ptr, hs_len - (ptr - start), ndl, ndl_len))
-// 	{
-// 		ptr = memmem(ptr, hs_len - (ptr - start), ndl, ndl_len);	// Find the next occurence of `ndl` in `hs` and set `ptr` to it
-// 		ptr += ndl_len;	// increment ptr by the length of `ndl` so we point after the found `ndl`
-// 		ndl_count++;	// increment ndl_count
-// 	}
-
-// 	//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&//
-// 	//&&&&&&&&&&&&&&&&&  REPLACE EVERY `ndl` in `hs`  &&&&&&&&&&&&&&&&&&&&//
-// 	//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&//
-// 	ptr = *hs;	// Reset the pointer to the start of `hs`
-// 	while (ndl_count > 0 && (ptr = memmem(ptr, hs_len - (ptr - start), ndl, ndl_len)))	// while ndl_count > 0 and `ndl` is found in `hs`
-// 	{
-// 		ptr = memmem(ptr, strlen(ptr), ndl, ndl_len);	// Find the next occurence of `ndl` in `hs` and set `ptr` to it
-// 		i = 0;
-// 		while (i < ndl_len)	// Replace ndl_len characters with '*' in `hs`
-// 		{
-// 			ptr[i] = '*';
-// 			i++;
-// 		}
-// 		ndl_count--;	// decrement ndl_count
-// 	}
-// }
-
-// /**
-//  * @brief `read()` sometimes fails to read big chunks of data in one go,
-//  * so we read in a loop until, memorising the total bytes read and appending all
-//  * non-read data to the buffer, until we read all the data from stdin. If `read()` returns 0
-//  *  */
-// int	main(int ac, char **av)
-// {
-// 	char	*buf = calloc(10000, sizeof(char));	// Allocate 10k bytes buffer to read from stdin
-// 	char	*hs;	// Heap string
-// 	char	*ndl;	// Needle
-// 	int bytes_read;	// Bytes read from stdin
-// 	int total_bytes_read = 0;	// Total bytes read from stdin
-
-// 	(void)ac;
-// 	while ((bytes_read = read(0, buf + total_bytes_read, 10000 - total_bytes_read)) > 0)
-// 	{
-// 		total_bytes_read += bytes_read;
-// 	}
-
-// 	if (bytes_read < 0)	// security check in case read() fails
-// 	{
-// 		fprintf(stderr, "Error: Can't read file\n");
-// 		free(buf);
-// 		return (1);
-// 	}
-// 	buf[total_bytes_read] = '\0'; // Null-terminate the buffer
-// 	if (!(*buf))
-// 		return (0);
-
-// 	hs = copy_str(buf);	// Copy the buffer to the heap string
-// 	free(buf);	// Free the buffer
-// 	if (!hs)	// security check in case memory allocation fails
-// 	{
-// 		fprintf(stderr, "Error: Memory allocation\n");
-// 		return (1);
-// 	}
-
-// 	ndl = copy_str(av[1]);	// Copy the needle from argv
-// 	if (*ndl)	// only run this if the needle is not empty
-// 		str_replace(&hs, strlen(hs), ndl, strlen(ndl));	// Replace all occurences of needle in the heap string
-// 	put_stdout(hs);	// Print the heap string to stdout
-// 	return (0);
-// }
 
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -148,104 +17,156 @@
 #include <unistd.h>
 
 // CAUTION: memmem() cannot be used anymore!
-//allowed functions as of Nov 21 2024: 
-//printf, perror, calloc, malloc, realloc, strlen, read, free 
+//allowed functions as of Nov 21 2024:
+//printf, perror, calloc, malloc, realloc, strlen, read, free
 
-/**
- * @brief Allocates and copies the contents of `str`.
- */
-char *copy_str(char *str)
+/*
+The program takes one argument `ndl` and starts by reading from stdin.
+Whatever it reads from stdin will now be refered to as `hs`.
+
+The program then must find and replace every occurence of `ndl` in `hs`
+and replace it with how ever many strlen(ndl) `*` (stars)
+
+It then must write the new string to stdout and return 0.
+
+If an error occurs it must exit and write "Error: " followed
+by an adequate error message in stderr and return 1.
+
+Allowed functions:
+printf, perror, calloc, malloc, realloc, strlen, read, free
+
+Usage example:
+$:echo "hello friend, hello foe" | ./filter "hello"
+***** friend, ***** foe$
+$:
+
+
+*/
+
+
+int	handle_error(char *errmsg)
 {
-    if (!str)
-        return NULL;
-    char *res = malloc(strlen(str) + 1);
-    if (!res)
-        return NULL;
-    for (int i = 0; str[i]; i++)
-        res[i] = str[i];
-    res[strlen(str)] = '\0';
-    return res;
+	perror(errmsg);
+	return(1);
 }
 
-/**
- * @brief Prints `str` followed by a newline to stdout.
- */
-void put_stdout(char *str)
+int	strings_match(const char *str1, const char *str2, size_t len)
 {
-    for (int i = 0; str[i]; i++)
-        write(1, &str[i], 1);
-    write(1, "\n", 1);
+	size_t	i = 0;
+	while (i < len)
+	{
+		if (str1[i] != str2[i]) //return false as soon as two characters don't match
+			return (0);
+		i++;
+	}
+	return (1);//	both strings match, return 1
 }
 
-/**
- * @brief Replaces all occurrences of `ndl` in `hs` with '*'.
- */
-void str_replace(char **hs, int hs_len, char *ndl, int ndl_len)
+char	*ft_replacestr(const char *buf, const char *ndl)
 {
-    char *ptr = *hs;
+	size_t	buf_len = strlen(buf);
+	size_t	ndl_len = strlen(ndl);
+	size_t	i, j, k;
+	char	*result;
 
-    while ((ptr = memmem(ptr, hs_len - (ptr - *hs), ndl, ndl_len)))
-    {
-        for (int i = 0; i < ndl_len; i++)
-            ptr[i] = '*';
-        ptr += ndl_len;
-    }
+	if (ndl_len == 0)
+		return ("\x01");
+
+	result = calloc(buf_len + 1,  sizeof(char));
+	if (!result)
+		handle_error("Error: Memory Allocation Failed\n");
+
+	i = 0;
+	j = 0;
+	while (i < buf_len)
+	{
+		if (i <= (buf_len - ndl_len) && strings_match(&buf[i], ndl, ndl_len))
+		{
+			//found matching string, replace ndl_len `*` (stars)
+			k = 0;
+			while(k < ndl_len)
+			{
+				result[j++] = '*';
+				k++;
+			}
+			//skip over replaced ndl
+			i += ndl_len;
+		}
+		else
+			//otherwise simply copy over the characters
+			result[j++] = buf[i++];
+	}
+	return (result);
 }
 
-/**
- * @brief Reads input from stdin into a dynamically allocated buffer.
- */
-char *read_input(void)
+int	main(int ac, char **av)
 {
-    char *buf = calloc(10000, sizeof(char));
-    if (!buf)
-        return NULL;
-    int bytes_read, total_bytes = 0;
+	char	*result;
+	char	*buf;
+	const char	*ndl;
+	size_t	buffer_size, total_read;
+	ssize_t	bytes_read;
 
-    while ((bytes_read = read(0, buf + total_bytes, 10000 - total_bytes)) > 0)
-        total_bytes += bytes_read;
+	if (ac > 2)//return error if more than two args
+		return (handle_error("Error: Too many arguments\n"));
 
-    if (bytes_read < 0)
-    {
-        free(buf);
-        return NULL;
-    }
-    buf[total_bytes] = '\0';
-    return buf;
-}
+	if (av[1])//point `ndl` to the substring in the user's input
+		ndl = av[1];
+	else
+		ndl = NULL;
 
-int main(int ac, char **av)
-{
-    if (ac < 2)
-    {
-        fprintf(stderr, "Usage: %s <needle>\n", av[0]);
-        return 1;
-    }
+	buffer_size = 1;
+	total_read = 0;
+	buf = malloc(buffer_size); //allocate memory for buf
+	if (!buf)
+		handle_error("Error: Memory Allocation Failed\n");
 
-    char *buf = read_input();
-    if (!buf)
-    {
-        fprintf(stderr, "Error: Can't read input\n");
-        return 1;
-    }
+	//read in a loop appending to buf with each iterration until 0 (end of file)
+	//add total_size to buffer each loop to ensure read doesn't overwrite data in buf
+	//substract total_size from buffer size to ensure you don't read out of bounds
+	while ((bytes_read = read(0, buf + total_read, buffer_size - total_read)) > 0)
+	{
+		total_read += bytes_read;//		keep track of total_size
 
-    char *hs = copy_str(buf);
-    free(buf);
-    if (!hs)
-    {
-        fprintf(stderr, "Error: Memory allocation failed\n");
-        return 1;
-    }
+//											dynamically allocate more memory if
+		if (total_read >= buffer_size)//	total_size gets bigger than buffer_size
+		{
+			buffer_size *= 2;
+			buf = realloc(buf, buffer_size);
+			if (!buf)
+				handle_error("Error: Memory Allocation Failed\n");
+		}
+	}
+	//return an error if read returned -1
+	if (bytes_read == -1)
+		handle_error("Error: failed to read input\n");
 
-    char *ndl = copy_str(av[1]);
-    if (*ndl)
-        str_replace(&hs, strlen(hs), ndl, strlen(ndl));
+	//If the user did not input a ndl (av[1])
+	//print `buf` and exit program cleanly
+	if (!ndl)
+	{
+		printf("%s\n", buf);
+		free(buf);
+		return (0);
+	}
 
-    put_stdout(hs);
+	//Null terminate string read from stdin
+	buf[total_read] = '\0';
 
-    free(hs);
-    free(ndl);
-    return 0;
+	//replace every occurence of `ndl` in `buf`
+	result = ft_replacestr(buf, ndl);
+
+	//return an error if result is NULL
+	if (!result)
+		handle_error("Error: Failed to write output\n");
+
+	//print the result to stdout
+	// printf("END\n");
+	printf("%s\n", result);
+	free(buf);
+	free(result);
+
+	return (0);
 }
 
 
