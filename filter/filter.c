4541a2ab5e8d157a5b3c29817721b4568749c9be
Mikan95 <andrewmeechan@hotmail.com>
2024-11-28 15:38:42 +0100

    Modified how the code functions when no needle is given by user, handle_error now properly exit (1) instead of return (1)

--- filter/filter.c
+++ filter/filter.c
@@ -6,7 +6,7 @@
 /*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 22:19:56 by ameechan          #+#    #+#             */
-/*   Updated: 2024/11/27 20:11:42 by ameechan         ###   ########.fr       */
+/*   Updated: 2024/11/28 15:36:54 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,18 +44,22 @@ $:
 */
 
 
-int	handle_error(char *errmsg)
+int	handle_error()
 {
-	perror(errmsg);
-	return(1);
+	perror("Error: ");
+	exit (1);
 }
 
-int	strings_match(const char *str1, const char *str2, size_t len)
+int	strings_match(const char *buf, const char *ndl, size_t len)
 {
+	//if ndl is NULL return false immediately
+	if (!ndl || len == 0)
+		return (0);
+
 	size_t	i = 0;
 	while (i < len)
 	{
-		if (str1[i] != str2[i]) //return false as soon as two characters don't match
+		if (buf[i] != ndl[i]) //return false as soon as two characters don't match
 			return (0);
 		i++;
 	}
@@ -65,16 +69,15 @@ int	strings_match(const char *str1, const char *str2, size_t len)
 char	*ft_replacestr(const char *buf, const char *ndl)
 {
 	size_t	buf_len = strlen(buf);
-	size_t	ndl_len = strlen(ndl);
+	size_t	ndl_len = 0;
 	size_t	i, j, k;
 	char	*result;
 
-	if (ndl_len == 0)
-		return ("\x01");
-
+	if (ndl)
+		ndl_len = strlen(ndl);
 	result = calloc(buf_len + 1,  sizeof(char));
 	if (!result)
-		handle_error("Error: Memory Allocation Failed\n");
+		handle_error();
 
 	i = 0;
 	j = 0;
@@ -108,23 +111,23 @@ int	main(int ac, char **av)
 	ssize_t	bytes_read;
 
 	if (ac > 2)//return error if more than two args
-		return (handle_error("Error: Too many arguments\n"));
+		return (handle_error());
 
 	if (av[1])//point `ndl` to the substring in the user's input
 		ndl = av[1];
 	else
 		ndl = NULL;
 
-	buffer_size = 1;
+	buffer_size = 20;
 	total_read = 0;
 	buf = malloc(buffer_size); //allocate memory for buf
 	if (!buf)
-		handle_error("Error: Memory Allocation Failed\n");
+		handle_error();
 
 	//read in a loop appending to buf with each iterration until 0 (end of file)
 	//add total_size to buffer each loop to ensure read doesn't overwrite data in buf
 	//substract total_size from buffer size to ensure you don't read out of bounds
-	while ((bytes_read = read(0, buf + total_read, buffer_size - total_read)) > 0)
+	while ((bytes_read = read(STDIN_FILENO, buf + total_read, buffer_size - total_read)) > 0)
 	{
 		total_read += bytes_read;//		keep track of total_size
 
@@ -134,21 +137,12 @@ int	main(int ac, char **av)
 			buffer_size *= 2;
 			buf = realloc(buf, buffer_size);
 			if (!buf)
-				handle_error("Error: Memory Allocation Failed\n");
+				handle_error();
 		}
 	}
 	//return an error if read returned -1
 	if (bytes_read == -1)
-		handle_error("Error: failed to read input\n");
-
-	//If the user did not input a ndl (av[1])
-	//print `buf` and exit program cleanly
-	if (!ndl)
-	{
-		printf("%s\n", buf);
-		free(buf);
-		return (0);
-	}
+		handle_error();
 
 	//Null terminate string read from stdin
 	buf[total_read] = '\0';
@@ -158,11 +152,10 @@ int	main(int ac, char **av)
 
 	//return an error if result is NULL
 	if (!result)
-		handle_error("Error: Failed to write output\n");
+		handle_error();
 
 	//print the result to stdout
-	// printf("END\n");
-	printf("%s\n", result);
+	printf("%s", result);
 	free(buf);
 	free(result);
 
